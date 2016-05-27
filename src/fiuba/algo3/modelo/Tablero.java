9ee3de85f9e365b9e9c380167fab090898a8bfd8
joakoracing <joakoracing@hotmail.com>
2016-05-27 16:13:20 -0300

    pequeño cambio

--- src/fiuba/algo3/modelo/Tablero.java
+++ src/fiuba/algo3/modelo/Tablero.java
@@ -2,7 +2,9 @@ package fiuba.algo3.modelo;
 import java.util.Random;
 
 public class Tablero {
-	private Casillero[][] casilleros = new Casillero[20][30];
+	static final int columnas = 30;
+	static final int filas = 20;
+	private Casillero[][] casilleros = new Casillero[filas][columnas];
 	
 	public void ubicarAutobot(Autobot autobot){
 		Random rnd = new Random();
@@ -15,7 +17,7 @@ public class Tablero {
 		Random rnd = new Random();
 		int i = (int)(rnd.nextDouble()*20);
 		//VER SI CASILLERO ESTA OCUPADO
-		this.casilleros[i][29].agregarAlgoformer(decepticon);
+		this.casilleros[i][columnas-1].agregarAlgoformer(decepticon);
 	}
 	
 	public void moverAlgoformer(){
