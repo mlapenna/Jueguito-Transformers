b9f7a8fc08fcf7f3def2a70be28234acc3ee6d82
nachox86 <nachox86@users.noreply.github.com>
2016-05-29 18:59:37 -0300

    Agrego métodos
    Son métodos que surgieron de escribir el test de "testAgregarAlgoformerHumanoideMoverYVerificarPosicion" y "testAgregarAlgoformerAlternoMoverYVerificarPosicion".

--- src/fiuba/algo3/modelo/Algoformer.java
+++ src/fiuba/algo3/modelo/Algoformer.java
@@ -1,9 +1,35 @@
 package fiuba.algo3.modelo;
 
+import fiuba.algo3.modelo.Posicion;
+import fiuba.algo3.modelo.Tablero;
+
 public abstract class Algoformer {
 	static final String modoHumanoide = "humanoide";
 	static final String modoAlterno = "alterno";
 	
 	protected int vida;
 	protected String nombre;
+	
+	
+	public void transformarHumanoide() {
+		
+	}
+	
+	public void transformarAlterno(){
+		
+	}
+	
+	public void agregarATablero(Algoformer transformer){
+		
+	}
+	
+	public void moverNuevaPosicion(Tablero tablero, Posicion posicion){
+		
+	}
+	
+	public Posicion obtenerPosicion(Tablero tablero){
+		Posicion posicion = new Posicion();
+		return posicion;		
+	}
+	
 }
