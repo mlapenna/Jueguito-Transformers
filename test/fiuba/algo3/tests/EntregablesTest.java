49ac316e5f1dad1391c106d53b391681877a7410
nachox86 <nachox86@users.noreply.github.com>
2016-05-29 18:58:10 -0300

    Archivo de pruebas entregables
    vayamos dividiendo por entregas en este archivo como muestra lo que agregué.
    Agregué una clase Posicion para no estar manipulando todo el tiempo una posicion x y una y.

---
+++ test/fiuba/algo3/tests/EntregablesTest.java
@@ -0,0 +1,55 @@
+package fiuba.algo3.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import fiuba.algo3.modelo.Optimus;
+import fiuba.algo3.modelo.Algoformer;
+import fiuba.algo3.modelo.Posicion;
+import fiuba.algo3.modelo.Tablero;
+
+public class EntregablesTest {
+	
+	// Primera entrega Jueves 02/06/2016 - Lunes 06/06/2016
+	
+	@Test
+	public void testAgregarAlgoformerHumanoideMoverYVerificarPosicion() {
+		
+		Tablero tablero = new Tablero();
+		Algoformer prime = new Optimus();
+		Posicion nuevaPosicion = new Posicion();
+		Posicion finalPosicion = new Posicion();
+		
+		nuevaPosicion.cargarPosicion(3,1);
+		
+		prime.transformarHumanoide();
+		prime.agregarATablero(prime);
+		prime.moverNuevaPosicion(tablero,nuevaPosicion);
+		
+		finalPosicion = prime.obtenerPosicion(tablero);
+		
+	}
+	
+	@Test
+	public void testAgregarAlgoformerAlternoMoverYVerificarPosicion() {
+		
+		Tablero tablero = new Tablero();
+		Algoformer prime = new Optimus();
+		Posicion nuevaPosicion = new Posicion();
+		Posicion finalPosicion = new Posicion();
+		
+		nuevaPosicion.cargarPosicion(3,1);
+		
+		prime.transformarAlterno();
+		prime.agregarATablero(prime);
+		prime.moverNuevaPosicion(tablero,nuevaPosicion);
+		
+		finalPosicion = prime.obtenerPosicion(tablero);
+		
+	}
+	
+	
+	
+	// 
+
+}
