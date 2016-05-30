4e1cb81253455fcee61ce20b0a730f02147f2bc2
joako <joakoracing@hotmail.com>
2016-05-30 15:59:05 -0300

    agrego metodos a Casillero

--- src/fiuba/algo3/modelo/Casillero.java
+++ src/fiuba/algo3/modelo/Casillero.java
@@ -2,7 +2,7 @@ package fiuba.algo3.modelo;
 
 public class Casillero {
 	
-	private Algoformer algoformer = null;
+	private Algoformer algoformer = null; //VER QUE ESTA FEO EL NULL ACA
 	private boolean vacio=true;
 	private Tierra tierra;
 	private Aire aire;
@@ -11,5 +11,26 @@ public class Casillero {
 	public void agregarAlgoformer(Algoformer algoformer) {
 		this.algoformer=algoformer;
 		this.vacio=false;
+	}
+	
+	public void quitarAlgoformer(Algoformer algoformer){
+		this.algoformer=null;
+		this.vacio=true;
+	}
+	
+	public boolean estaVacio(){
+		return this.vacio;
+	}
+	
+	public void agregarSuperficieTerrestre(Tierra tierra){
+		this.tierra=tierra;
+	}
+	
+	public void agregarSuperficieAerea(Aire aire){
+		this.aire=aire;
+	}
+	
+	public void agregarBonus(Bonus bonus){
+		this.bonus=bonus;
 	}
 }
