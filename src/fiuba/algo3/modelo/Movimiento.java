1d3053bc8f893b15c2383843d239f87379a9b8f3
nachox86 <nachox86@users.noreply.github.com>
2016-06-01 00:16:50 -0300

    creo clase Movimiento
    encargada de indicarle al teclado que haga el movimiento y de validar el mismo

---
+++ src/fiuba/algo3/modelo/Movimiento.java
@@ -0,0 +1,67 @@
+package fiuba.algo3.modelo;
+
+import fiuba.algo3.modelo.Posicion;
+
+import java.util.Calendar;
+
+import fiuba.algo3.modelo.Algoformer;
+import fiuba.algo3.modelo.Vacio;
+import fiuba.algo3.modelo.Tablero;
+import fiuba.algo3.modelo.Casillero;
+import fiuba.algo3.modelo.MovimientoInvalidoCasilleroOcupado;
+
+public class Movimiento {
+	
+	Tablero tablero;
+
+	public void moverPosicionAlgoformer(Algoformer algoformer,Posicion nuevaPosicion){
+		// Acá tiene que preguntar al teclado:
+		// está ocupada la posicion final?
+		// es movimiento válido?
+			// tiene bonus el algoformer?
+			// qué efecto tiene la superficie donde está parado el algoformer?
+		
+		// Ver q la posición que se quiere poner el algoformer, no esté ocupada
+		Casillero nuevoCasillero = this.obtenerCasillero(nuevaPosicion);
+		movimientoACasilleroVacio(nuevoCasillero);
+		
+		// Hay que ver si le da la distancia/velocidad que quiere
+		// Esto se hace con una diferencia entre las posiciones (cantidad de casilleros) y la velocidad 
+		// del algoformer. 
+		// Si la Posicion que quiero que tenga es mayor a la velocidad (cantidad de casillas máximas de movimiento)
+		// entonces no se puede mover.
+		try{
+			distanciaDeMovimientoValida(algoformer,nuevaPosicion);
+		}
+		catch(Exception e)
+		{
+			throw new MovimientoInvalidoDistanciaNoValida();
+		}
+		
+	}
+	
+	private Casillero obtenerCasillero(Posicion nuevaPoscion){
+		// TODO: buscar el casillero en el tablero
+		return new Casillero();
+	}
+	
+	private void movimientoACasilleroVacio(Casillero casillero){
+		
+		Vacio contenidoVacio = new Vacio();
+		try{ //TODO
+			// comparacion del contenido del casillero con vacio.
+			//if(nuevoCasillero.obtenerContenido() == contenidoVacio) {
+			//}
+		}
+		catch(Exception e)
+		{
+			throw new MovimientoInvalidoCasilleroOcupado();
+		}
+	}
+	
+	private void distanciaDeMovimientoValida(Algoformer algoformer,Posicion destino){
+		// TODO
+		// Acá tomo la velocidad que tenga el algoformer y me fijo hasta donde puede ir
+		
+	}
+}
