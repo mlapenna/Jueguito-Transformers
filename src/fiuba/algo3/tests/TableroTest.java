dfb58aa36deff951a02678fd629df1453b8ec25f
nachox86 <nachox86@users.noreply.github.com>
2016-06-04 19:26:46 -0300

    Corre el test

--- src/fiuba/algo3/tests/TableroTest.java
+++ src/fiuba/algo3/tests/TableroTest.java
@@ -20,26 +20,22 @@ import java.io.FileNotFoundException;
 public class TableroTest {
 
 	@Test
-	public void testAgregarAlgoformerHumanoideSinEfectoDeSuperficie ()  throws FileNotFoundException, 
+	public void testAgregarAlgoformerHumanoideSinEfectoDeSuperficie () throws FileNotFoundException, 
 	 IOException, ParseException {
-		
 		JSONParser parser = new JSONParser();
 		JSONObject jsonTablero = (JSONObject) parser.parse(new FileReader("mapas/1.json"));
 		Tablero tablero = new Tablero(jsonTablero);
 		Algoformer algoformer = new Optimus();
-
 		Posicion nuevaPosicion = new Posicion();
-
+		Posicion algoformerPosicion = new Posicion();
 		nuevaPosicion.cargarPosicion(3,1);
-
-		tablero.agregarAlgoformerHumanoideSinEfectoDeSuperficie(algoformer, nuevaPosicion);
-		Posicion algoformerPosicion = algoformer.obtenerPosicion();
-
-		System.out.println(algoformer.obtenerPosicion().obtenerPosicionX());
-		System.out.println(algoformer.obtenerPosicion().obtenerPosicionY());
 		
-		Assert.assertTrue( nuevaPosicion.obtenerPosicionX() == algoformerPosicion.obtenerPosicionX() );
-		Assert.assertTrue( nuevaPosicion.obtenerPosicionY() == algoformerPosicion.obtenerPosicionY() );
+		tablero.agregarAlgoformerHumanoideSinEfectoDeSuperficie(algoformer,nuevaPosicion);
+		algoformerPosicion = algoformer.obtenerPosicion();
+
+		Assert.assertTrue(nuevaPosicion.obtenerPosicionX()==algoformerPosicion.obtenerPosicionX());
+		Assert.assertTrue(nuevaPosicion.obtenerPosicionY()==algoformerPosicion.obtenerPosicionY());		
+		
 	}
 	
 	
