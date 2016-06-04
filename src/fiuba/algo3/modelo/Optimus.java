427183b533fae779ae21128c0516430f476644ee
nachox86 <cignaciogarcia@gmail.com>
2016-06-04 19:55:38 -0300

    cambio valor de velocidad modo alterno que no era la debia

--- src/fiuba/algo3/modelo/Optimus.java
+++ src/fiuba/algo3/modelo/Optimus.java
@@ -2,6 +2,7 @@ package fiuba.algo3.modelo;
 
 import java.util.Calendar;
 import fiuba.algo3.modelo.TransformacionIncorresctaYaEsHumanoide;
+import fiuba.algo3.modelo.TransformacionIncorresctaYaEsAlterno;
 
 public class Optimus extends Autobot {
 	private static final int ATAQUE_HUMANOIDE = 50;
@@ -9,7 +10,7 @@ public class Optimus extends Autobot {
 	private static final int VELOCIDAD_HUMANOIDE = 2;
 	private static final int ATAQUE_ALTERNO = 15;
 	private static final int DISTANCIA_ATAQUE_ALTERNO = 4;
-	private static final int VELOCIDAD_ALTERNO = 2;
+	private static final int VELOCIDAD_ALTERNO = 5;
 	private static final int VIDA = 500;
 	
 	public Optimus() {
@@ -19,10 +20,6 @@ public class Optimus extends Autobot {
 		this.velocidad = VELOCIDAD_HUMANOIDE;
 		this.movimiento = new MovimientoHumanoide();
 	}
-	
-//	private int ataque = ATAQUE_HUMANOIDE;
-//	private int distanciaDeAtaque = DISTANCIA_ATAQUE_HUMANOIDE;
-//	private int velocidad = VELOCIDAD_HUMANOIDE;
 	
 	public void transformarHumanoide() {
 		
@@ -36,9 +33,12 @@ public class Optimus extends Autobot {
 	}
 	
 	public void transformarAlterno(){
-		if(this.ataque == ATAQUE_ALTERNO || this.distanciaDeAtaque == DISTANCIA_ATAQUE_ALTERNO || this.velocidad == VELOCIDAD_ALTERNO)
-			throw new TransformacionIncorresctaYaEsHumanoide();
-		
+
+		if((this.ataque == ATAQUE_ALTERNO) || (this.distanciaDeAtaque == DISTANCIA_ATAQUE_ALTERNO) || (this.velocidad == VELOCIDAD_ALTERNO))
+		{
+			throw new TransformacionIncorresctaYaEsAlterno();
+		}
+
 		this.movimiento = new MovimientoAlternoTerrestre();
 		this.ataque = ATAQUE_ALTERNO;
 		this.distanciaDeAtaque = DISTANCIA_ATAQUE_ALTERNO;
