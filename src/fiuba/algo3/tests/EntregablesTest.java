fe60839c2a765029536f843d1c21fc5177f4fe5b
nachox86 <cignaciogarcia@gmail.com>
2016-06-04 16:39:23 -0300

    Reformulo el test testAgregarAlgoformerHumanoideMoverYVerificarPosicion

--- src/fiuba/algo3/tests/EntregablesTest.java
+++ src/fiuba/algo3/tests/EntregablesTest.java
@@ -1,5 +1,13 @@
 package fiuba.algo3.tests;
 
+import java.io.FileReader;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,32 +15,44 @@ import fiuba.algo3.modelo.Optimus;
 import fiuba.algo3.modelo.Algoformer;
 import fiuba.algo3.modelo.Posicion;
 import fiuba.algo3.modelo.Tablero;
+import fiuba.algo3.modelo.TransformacionIncorresctaYaEsHumanoide;
+import fiuba.algo3.modelo.TransformacionIncorresctaYaEsAlterno;
 
 public class EntregablesTest {
 	
 	// Primera entrega Jueves 02/06/2016 - Lunes 06/06/2016
 	
 	@Test
-	public void testAgregarAlgoformerHumanoideMoverYVerificarPosicion() {
-		// Como no está terminado, para que no de error al correr los tests lo comento
-		/*Tablero tablero = new Tablero();
+	public void testAgregarAlgoformerHumanoideMoverYVerificarPosicion() throws FileNotFoundException, 
+	 IOException, ParseException {
+
+		JSONParser parser = new JSONParser();
+		JSONObject jsonTablero = (JSONObject) parser.parse(new FileReader("mapas/mapa.json"));
+		Tablero tablero = new Tablero(jsonTablero);
 		Algoformer prime = new Optimus();
 		Posicion nuevaPosicion = new Posicion();
-		Posicion finalPosicion = new Posicion();
+		Posicion ultimaPosicion = new Posicion();
+		Posicion auxPosicion = new Posicion();
 		
 		nuevaPosicion.cargarPosicion(3,1);
+		ultimaPosicion.cargarPosicion(5,1);
 		
-		prime.transformarHumanoide();
-		prime.agregarATablero(prime);
-		prime.moverNuevaPosicion(tablero,nuevaPosicion);
+		try {prime.transformarHumanoide();}
+		catch(Exception e){}
 		
-		finalPosicion = prime.obtenerPosicion(tablero);*/
+		tablero.agregarAlgoformerHumanoideSinEfectoDeSuperficie(prime,nuevaPosicion);
+		tablero.moverAlgoformerHumanoide(prime,ultimaPosicion);
+				
+		auxPosicion = prime.obtenerPosicion();
+		
+		Assert.assertTrue(auxPosicion.obtenerPosicionX()==ultimaPosicion.obtenerPosicionX());
+		Assert.assertTrue(auxPosicion.obtenerPosicionY()==ultimaPosicion.obtenerPosicionY());
 		
 	}
 	
 	@Test
 	public void testAgregarAlgoformerAlternoMoverYVerificarPosicion() {
-		// Como no está terminado, para que no de error al correr los tests lo comento
+		// Como no estï¿½ terminado, para que no de error al correr los tests lo comento
 		/*Tablero tablero = new Tablero();
 		Algoformer prime = new Optimus();
 		Posicion nuevaPosicion = new Posicion();
@@ -50,7 +70,7 @@ public class EntregablesTest {
 	
 	@Test
 	public void testAgregarAlgoformerYCambiarDeModoEnAmbasDirecciones() {
-		// Como no está terminado, para que no de error al correr los tests lo comento
+		// Como no estï¿½ terminado, para que no de error al correr los tests lo comento
 		/*Algoformer prime = new Optimus();
 		
 		prime.transformarAlterno();
