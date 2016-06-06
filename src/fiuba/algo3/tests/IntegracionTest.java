eed2551af98847ca881c2af550a6c8376032e63b
nachox86 <cignaciogarcia@gmail.com>
2016-06-05 21:30:40 -0300

    dejo la clase vacia

--- src/fiuba/algo3/tests/IntegracionTest.java
+++ src/fiuba/algo3/tests/IntegracionTest.java
@@ -16,24 +16,6 @@ import java.io.IOException;
 
 public class IntegracionTest {
 
-	@Test
-	public void testJugar() {
-		try {
-			JSONParser parser = new JSONParser();
-			JSONObject jsonTablero = (JSONObject)parser.parse(new FileReader("mapas/1.json"));
-			Tablero tablero = new Tablero(jsonTablero);
-			Jugador jugador1 = new Jugador("Pedro", Algoformer.MODO_AUTOBOT, tablero);
-			Jugador jugador2 = new Jugador("Juan", Algoformer.MODO_DECEPTICON, tablero);
-			Juego juego = new Juego(jugador1, jugador2); // El juego crea al azar 3 robots diferentes para cada jugador y los posiciona
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-
-
-	}
+	
 
 }
