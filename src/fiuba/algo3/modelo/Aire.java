990ae8154f5af7727a93deaff473b33ab5ae7def
nachox86 <nachox86@users.noreply.github.com>
2016-06-07 21:18:36 -0300

    movido a superficies

--- src/fiuba/algo3/modelo/Aire.java
+++
@@ -1,8 +0,0 @@
-package fiuba.algo3.modelo;
-
-public class Aire extends Superficie{
-	static final String NUBE="nube";
-	static final String NEBULOSA_DE_ANDROMEDA ="nebulosaDeAndromeda";
-	static final String TORMENTA_PSIONICA="tormentaPsiconica";
-
-}
