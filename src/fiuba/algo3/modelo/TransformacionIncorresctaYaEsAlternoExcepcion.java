d60cbde13b5893c07dafffaa6bc825f0e2e4b560
nachox86 <nachox86@users.noreply.github.com>
2016-06-07 21:22:32 -0300

    movido a excepciones

--- src/fiuba/algo3/modelo/TransformacionIncorresctaYaEsAlternoExcepcion.java
+++
@@ -1,7 +0,0 @@
-package fiuba.algo3.modelo;
-
-public class TransformacionIncorresctaYaEsAlternoExcepcion extends RuntimeException {
-    public TransformacionIncorresctaYaEsAlternoExcepcion() {
-        super();
-    }
-}
