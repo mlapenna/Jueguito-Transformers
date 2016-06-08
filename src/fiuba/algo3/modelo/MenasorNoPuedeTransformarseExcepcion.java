047f313d57cb2bb9dddb1aaf8cacb9393dbe5e91
nachox86 <nachox86@users.noreply.github.com>
2016-06-07 21:21:27 -0300

    movido a excepciones

--- src/fiuba/algo3/modelo/MenasorNoPuedeTransformarseExcepcion.java
+++
@@ -1,7 +0,0 @@
-package fiuba.algo3.modelo;
-
-public class MenasorNoPuedeTransformarseExcepcion extends RuntimeException {
-    public MenasorNoPuedeTransformarseExcepcion() {
-        super();
-    }
-}
