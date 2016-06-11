3c93ab35759207fb071319928e7f78a341af9694
joako <joakoracing@hotmail.com>
2016-06-11 16:28:19 -0300

    saco movimiento alterno xq estaba al pedo

--- src/fiuba/algo3/modelo/movimientos/MovimientoAlternoAereo.java
+++ src/fiuba/algo3/modelo/movimientos/MovimientoAlternoAereo.java
@@ -1,11 +1,10 @@
 package fiuba.algo3.modelo.movimientos;
 
-import fiuba.algo3.modelo.movimientos.MovimientoAlterno;
 import fiuba.algo3.modelo.algoformers.Algoformer;
 import fiuba.algo3.modelo.Posicion;
 import fiuba.algo3.modelo.Tablero;
 
-public class MovimientoAlternoAereo extends MovimientoAlterno{
+public class MovimientoAlternoAereo extends Movimiento{
 
 	@Override
 	public void afectarAlgoformer(Algoformer algoformer, Tablero tablero, Posicion posicion) {
