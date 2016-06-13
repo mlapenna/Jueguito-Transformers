57fe5fe3faf6fa162333d1f9e060dc284eb3d263
mlapenna <mlapenna79@hotmail.com>
2016-06-12 23:36:15 -0300

    Muevo test desde el pack de la 2da entrega hacia los de integración.

--- src/fiuba/algo3/tests/integracion/IntegracionTest.java
+++ src/fiuba/algo3/tests/integracion/IntegracionTest.java
@@ -1,8 +1,10 @@
 package fiuba.algo3.tests.integracion;
 
+import fiuba.algo3.modelo.Posicion;
 import fiuba.algo3.modelo.algoformers.Algoformer;
 import fiuba.algo3.modelo.Jugador;
 import fiuba.algo3.modelo.Tablero;
+import fiuba.algo3.modelo.algoformers.Megatron;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -16,6 +18,24 @@ import java.io.IOException;
 
 public class IntegracionTest {
 
-	
+    @Test
+    public void testLlenarZonaPantanoYVerificarEnModoHumanoideNoPasaYVolverACondicionesIniciales() throws IOException, ParseException{
+
+        JSONParser parser = new JSONParser();
+        JSONObject jsonTablero = (JSONObject) parser.parse(new FileReader("mapas/mapaTestZonaPantano.json"));
+        Tablero tablero = new Tablero(jsonTablero);
+
+        Posicion posicion = new Posicion(3, 0);
+        Algoformer megatron = new Megatron(posicion,tablero);
+        Posicion posicionDestino = new Posicion(2,0);
+
+        try{
+            megatron.mover(posicionDestino);
+        }catch (Exception MovimientoInvalidoIncapazDeAtravezarSuperficieExcepcion){
+            Assert.assertEquals(megatron.getPosicion(), posicion);
+            Assert.assertEquals(tablero.getCasillero(posicion).getContenido(), megatron);
+            Assert.assertTrue(tablero.getCasillero(posicionDestino).estaVacio());
+        }
+    }
 
 }
