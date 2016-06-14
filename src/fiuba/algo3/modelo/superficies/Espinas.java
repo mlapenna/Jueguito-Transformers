309c1c9252ec3e2fe9e54c9339a8f95e251e0583
mlapenna <mlapenna79@hotmail.com>
2016-06-14 15:48:55 -0300

    Unifico código duplicado

--- src/fiuba/algo3/modelo/superficies/Espinas.java
+++ src/fiuba/algo3/modelo/superficies/Espinas.java
@@ -6,20 +6,21 @@ public class Espinas extends Tierra {
 	public static String SUPERFICIE = "espinas";
 	public static final String NOMBRE_JSON = "Espinas";
 	public static final double DANIO = 0.05;
-	
-	@Override
-	public void afectarAlgoformerHumanoide(Algoformer algoformer) {
-		
+
+	private void afectarAlgoformerAmbosModos(Algoformer algoformer) {
 		double vida = (double) algoformer.getVida();
 		int vidaAux = (int) ( vida - ( DANIO * vida ) );
 		algoformer.afectarVida(vidaAux);
 	}
 
 	@Override
+	public void afectarAlgoformerHumanoide(Algoformer algoformer) {
+		this.afectarAlgoformerAmbosModos(algoformer);
+	}
+
+	@Override
 	public void afectarAlgoformerAlterno(Algoformer algoformer) { //FUNCIION PRIVADA!!!
-		double vida = (double) algoformer.getVida();
-		int vidaAux = (int) ( vida - ( DANIO * vida ) );
-		algoformer.afectarVida(vidaAux);
+		this.afectarAlgoformerAmbosModos(algoformer);
 	}
 	
 }
