46c6b9ab1d12bca0bbba90dfff0159ff6d92c54b
nachox86 <cignaciogarcia@gmail.com>
2016-06-20 22:21:57 -0300

    vuelta atras

--- src/fiuba/algo3/modelo/Tablero.java
+++ src/fiuba/algo3/modelo/Tablero.java
@@ -1,14 +1,9 @@
 package fiuba.algo3.modelo;
 
 import java.util.ArrayList;
-import java.io.FileReader;
-import javafx.scene.Node;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import java.io.IOException;
 import org.json.simple.JSONArray;
-import fiuba.algo3.modelo.TableroSkin;
+
 
 public class Tablero {
 
@@ -16,35 +11,19 @@ public class Tablero {
 	
 	private int dimensionX;
 	private int dimensionY;
-	private final TableroSkin skin;
-	private final Square[][] squaresTierra;
-	private final Square[][] squaresAire;
-	private final Square[][] squares;
-	private JSONObject jsonTablero;
-	private JSONArray jsonCasilleros;
-	private Juego game;
-	
+
 	private static final String JSON_FIELD_KEY_DIMENSION_X = "dimensionX";
 	private static final String JSON_FIELD_KEY_DIMENSION_Y = "dimensionY";
 	private static final String JSON_FIELD_KEY_CASILLEROS = "casilleros";
 	private static final String JSON_FIELD_KEY_POSICION_X = "posicionX";
 	private static final String JSON_FIELD_KEY_POSICION_Y = "posicionY";
 	private static final String JSON_FIELD_KEY_SUPERFICIES = "superficies";
-	private static final String JSON_FIELD_KEY_SUPERFICIE_TIERRA = "tierra";
-	private static final String JSON_FIELD_KEY_SUPERFICIE_AIRE = "aire";
 
 	public Tablero(JSONObject json) {
 
-		try{
-			JSONParser parser = new JSONParser();
-			this.jsonTablero = (JSONObject) parser.parse(new FileReader("mapas/1.json"));
-		} catch (IOException ioexception){System.out.println("Salto la excepcion IO.");} catch (ParseException parseexception){System.out.println("Salto la excepcion Parse.");};
-		this.loadBoardSize();
-		this.squaresTierra = new Square[this.dimensionX][this.dimensionY];
-		this.squaresAire = new Square[this.dimensionX][this.dimensionY];
-		this.constructBoard();
-		this.squares = this.squaresTierra;
-		this.skin = new TableroSkin(this);
+		this.dimensionX = Integer.parseInt( json.get(JSON_FIELD_KEY_DIMENSION_X).toString() );
+		this.dimensionY = Integer.parseInt( json.get(JSON_FIELD_KEY_DIMENSION_Y).toString() );
+
 
 		// Se crea cada casillero
 		for (int i = 0; i < this.dimensionY; i++) {
@@ -54,46 +33,22 @@ public class Tablero {
 			}
 			this.casilleros.add(fila);
 		}
+
+		// Por cada casillero del mapa, se guardan sus superficies
+		JSONArray casilleros = (JSONArray) json.get(JSON_FIELD_KEY_CASILLEROS);
+
+		for (int i = 0; i < casilleros.size(); i++) {
+			JSONObject unCasillero = (JSONObject) casilleros.get(i);
+
+			int posicionX = Integer.parseInt( unCasillero.get(JSON_FIELD_KEY_POSICION_X).toString() );
+			int posicionY = Integer.parseInt( unCasillero.get(JSON_FIELD_KEY_POSICION_Y).toString() );
+			JSONObject superficies = (JSONObject) unCasillero.get(JSON_FIELD_KEY_SUPERFICIES);
+
+			this.casilleros.get(posicionY).get(posicionX).setSuperficies(superficies);
+
+		}
 	}
 
-	private void loadBoardSize() {
-		 this.dimensionX = Integer.parseInt( this.jsonTablero.get(JSON_FIELD_KEY_DIMENSION_X).toString() );
-		 this.dimensionY = Integer.parseInt( this.jsonTablero.get(JSON_FIELD_KEY_DIMENSION_Y).toString() );
-	}
-	
-	 private void constructBoard() {
-		  int posicionX;
-		  int posicionY;
-		  JSONObject superficies;
-		  String superficieTierra;
-		  String superficieAire;
-		  
-		  JSONArray jsonCasilleros = (JSONArray) jsonTablero.get(JSON_FIELD_KEY_CASILLEROS);
-		  for (int i = 0; i < jsonCasilleros.size(); i++) {
-			  JSONObject unCasillero = (JSONObject) jsonCasilleros.get(i);
-			  posicionX = Integer.parseInt( unCasillero.get(JSON_FIELD_KEY_POSICION_X).toString() );
-			  posicionY = Integer.parseInt( unCasillero.get(JSON_FIELD_KEY_POSICION_Y).toString() );
-			  superficies = (JSONObject) unCasillero.get(JSON_FIELD_KEY_SUPERFICIES);
-			  superficieTierra = superficies.get(JSON_FIELD_KEY_SUPERFICIE_TIERRA).toString();
-			  superficieAire = superficies.get(JSON_FIELD_KEY_SUPERFICIE_AIRE).toString();
-			  this.squaresTierra[posicionX][posicionY] = new Square(game,superficieTierra);
-			  this.squaresAire[posicionX][posicionY] = new Square(game,superficieAire);
-		  }
-		  
-		  for (int i = 0; i < this.dimensionY; i++) {
-			ArrayList<Casillero> fila = new ArrayList<Casillero>();
-			for (int j=0; j < this.dimensionX ; j++) {
-				fila.add( new Casillero() );
-			}
-			this.casilleros.add(fila);
-		  }
-		  
-	  }
-	
-	public void getJuego(Juego game){
-		this.game = game;
-	}
-	 
 	public Casillero getCasillero(Posicion posicion) { //VER SI FUNCIONA
 		ArrayList<Casillero> fila = this.casilleros.get( posicion.getY() );
 		Casillero unCasillero = fila.get( posicion.getX() );
@@ -123,12 +78,5 @@ public class Tablero {
 	}
 
 
-	public Square getSquare(int i, int j) {
-		return squares[i][j];
-	}
-
-	public Node getSkin() {
-		 return skin;
-	}
 
 }
